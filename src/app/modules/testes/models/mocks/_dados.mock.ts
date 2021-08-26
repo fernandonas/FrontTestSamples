cddddcc11a43ae92c255d0486911f7d7673c8e18
Fernando August do Nascimento <fernando.nascimento@ambevtech.com.br>
2021-08-26 09:51:07 -0300

    Adicionado teste da service

---
+++ src/app/modules/testes/models/mocks/_dados.mock.ts
@@ -0,0 +1,6 @@
+import { Dados } from "../dados.model";
+
+export const dadosMock: Dados = {
+    id: 1,
+    texto: 'Texto'
+}
--- src/app/modules/testes/service/test.service.spec.ts
+++ src/app/modules/testes/service/test.service.spec.ts
@@ -1,10 +1,13 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Observable } from 'rxjs';
+import { Dados } from '../models/dados.model';
+import { dadosMock } from '../models/mocks/_dados.mock';
 import { TestService } from './test.service';
 
 describe('TestService', () => {
-  let service: TestService;
+  let testService: TestService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +15,26 @@ describe('TestService', () => {
         HttpClientTestingModule
       ]
     });
-    service = TestBed.inject(TestService);
+    testService = TestBed.inject(TestService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(testService).toBeTruthy();
+  });
+
+  it('Deve retornar os Dados', () => {
+    testService.retornaListaDeDados().subscribe({
+      next: (response: Dados[]) => {
+        expect(response).toEqual([dadosMock])
+      }
+    });
+    const req = httpTestingController.expectOne(`http://localhost:9000/dados`);
+    expect(req.request.method).toEqual('GET');
+    req.flush([dadosMock]);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
